cb17e106e86107d992daad7092e8ced4055c67f9
Capti <captianbeau@gmail.com>
2024-01-24 23:36:39 -0700

    added routes for html

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
 const noteData = require('./db/db.json');
 
 
 const app = express();
 const PORT = 3001;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/', (res, req) => )
+app.get('/', (res, req) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', (res,req) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes',(res,req) => res.json(noteData));
