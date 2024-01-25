47ef225ab52887241dc558c353f9c713361b415b
Capti <captianbeau@gmail.com>
2024-01-25 00:04:39 -0700

    attempted Get for readfile

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs')
 const noteData = require('./db/db.json');
-
+const api = require('./public/assets/js/index.js')
 
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+app.use('/api', api);
 
 app.get('/', (res, req) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -17,4 +21,16 @@ app.get('/notes', (res,req) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('/api/notes',(res,req) => res.json(noteData));
+app.get('/api/notes',(res,req) => {
+fs.readFile('./db/db.json', 'utf8', (err,data) =>{
+    if(err){
+        console.error(err);
+    }else {
+       const parsedNotes = json.parse(data);
+
+        return parsedNotes;
+    }
+})
+});
+
+app.post('/api/notes', )
