8645ad0be9918cadd1f98ed536461b8875c1bb63
Capti <captianbeau@gmail.com>
2024-02-21 12:34:57 -0700

    added comments to server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const uuid = require('uuid')
 //file require
 const noteData = require('./db/db.json');
 
-//
+//host
 const app = express();
 const PORT = 3001;
 
@@ -18,20 +18,20 @@ app.use(express.static('public'));
 
 
 //Home page index.html
-app.get('/', (req,res ) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // /notes is notes.html
-app.get('/notes', (req,res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-// read and return the notes REVIEW
+// read and return the notes
 app.get('/api/notes', (req, res) => {
 
     console.log(`${req.method} request received to get notes`);
-    
+
 
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
@@ -43,7 +43,7 @@ app.get('/api/notes', (req, res) => {
     })
 });
 
-// add new notes to db REVIEW
+// add new notes to db 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} add note request`);
 
@@ -55,7 +55,7 @@ app.post('/api/notes', (req, res) => {
             text,
             id: uuid.v4()
         };
-
+        //reads db and rewrites it with the new note
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
@@ -65,10 +65,10 @@ app.post('/api/notes', (req, res) => {
                 parsedNotes.push(newNote);
 
                 fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null),
-                (err) =>
-                    err
-                        ? console.error(err)
-                        : console.log('New Note added')
+                    (err) =>
+                        err
+                            ? console.error(err)
+                            : console.log('New Note added')
                 );
             }
         });
@@ -84,7 +84,10 @@ app.post('/api/notes', (req, res) => {
 
     }
 })
-app.get('/api/notes/:id', (req,res) =>{
+
+//get note by id
+app.get('/api/notes/:id', (req, res) => {
+
     const id = req.params.id;
 
     fs.readFile('/db/db.json', 'utf8', (err, data) => {
@@ -95,12 +98,13 @@ app.get('/api/notes/:id', (req,res) =>{
             res.status(200).json(parsedNotes)
             const title = parsedNotes.json.filter((title) => title.id === id)
             const text = parsedNotes.json.filter((text) => text.id === id)
-            const activeNote = {title, text}
+            const activeNote = { title, text }
             return activeNote.id;
-        } 
+        }
     })
 })
 
+//starts the application
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
 
